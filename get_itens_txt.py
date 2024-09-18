a7e5bc848ff6b686489a2a8250dc762910ed5633
gAlvim <ledyzito@hotmail.com>
2024-09-18 14:45:28 -0300

    Rascunho básico

---
+++ get_itens_txt.py
@@ -0,0 +1,13 @@
+class GetItensTxt:
+    def __init__(self,itens) -> None:
+        self._itens = list()
+    @property
+    def itens(self):
+        return self._itens
+    
+    @itens.setter
+    def itens(self,valor):
+        ...
+        
+        
+        
