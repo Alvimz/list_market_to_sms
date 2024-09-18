b01128ab9360eef598185742a4bfe581a7ac2823
gAlvim <ledyzito@hotmail.com>
2024-09-18 15:24:05 -0300

    Add:getter

--- get_itens_txt.py
+++ get_itens_txt.py
@@ -1,18 +1,21 @@
 class GetItensTxt:
     def __init__(self) -> None:
-        self.itens = list()
-        
+        self._itens = list()
+    
+    @property
+    def itens(self):
+        return self._itens    
     
     def get_itens(self):
         with open('itens_comprar.txt','r',encoding='utf-8') as file:
             
             for line in file:
                 item, qnt = line.strip().split(',')
-                self.itens.append((f'Item:{item},Qnt:{qnt}'))
+                self._itens.append((f'Item:{item},Qnt:{qnt}'))
             
-    def exibir_lista_compras(self):
+    def show_itens_in_list(self):
         print(10*'-')
-        for item in self.itens:
+        for item in self._itens:
             
             print(item)
             
@@ -24,7 +27,7 @@ class GetItensTxt:
 if __name__ == '__main__':
     a = GetItensTxt()
     a.get_itens()
-    a.exibir_lista_compras()
+    a.show_itens_in_list()
     
    
     
