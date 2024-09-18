f55fb2b8a3a479409d196234b3025608d023b3d9
gAlvim <ledyzito@hotmail.com>
2024-09-18 15:19:55 -0300

    Add:exibir lista de compras

--- get_itens_txt.py
+++ get_itens_txt.py
@@ -1,7 +1,7 @@
 class GetItensTxt:
     def __init__(self) -> None:
         self.itens = list()
-        self.qnt = list
+        
     
     def get_itens(self):
         with open('itens_comprar.txt','r',encoding='utf-8') as file:
@@ -10,6 +10,13 @@ class GetItensTxt:
                 item, qnt = line.strip().split(',')
                 self.itens.append((f'Item:{item},Qnt:{qnt}'))
             
+    def exibir_lista_compras(self):
+        print(10*'-')
+        for item in self.itens:
+            
+            print(item)
+            
+        print(10*'-')
                 
             
                 
@@ -17,7 +24,7 @@ class GetItensTxt:
 if __name__ == '__main__':
     a = GetItensTxt()
     a.get_itens()
-    print(a.itens)
+    a.exibir_lista_compras()
     
    
     
